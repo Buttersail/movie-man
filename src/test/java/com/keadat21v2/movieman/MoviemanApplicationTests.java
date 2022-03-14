338d1694f3bd5269b3327ce17c181e31396a372f
Simon Roswall <simonroswall@gmail.com>
2022-03-14 11:33:15 +0100

    project init with spring initialzr

---
+++ src/test/java/com/keadat21v2/movieman/MoviemanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keadat21v2.movieman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviemanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
