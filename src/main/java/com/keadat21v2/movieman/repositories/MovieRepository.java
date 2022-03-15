ab65f362be321ef7b4bd7a0c6148dd848d710ff6
Murat Memis <3605716+Buttersail@users.noreply.github.com>
2022-03-15 13:10:05 +0100

    Delete src directory

--- src/main/java/com/keadat21v2/movieman/repositories/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.keadat21v2.movieman.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//TODO change JPA parameter to Movie.
-public interface MovieRepository extends JpaRepository<Integer, Integer> {
-}
