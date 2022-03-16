75291fbd76e38fc26947bed8ad205baf424d5444
Simon Roswall <simonroswall@gmail.com>
2022-03-16 12:26:39 +0100

    add POC of http in java

--- src/main/java/dk/keadat21v2/movieman/services/Fetcher.java
+++ src/main/java/dk/keadat21v2/movieman/services/Fetcher.java
@@ -1,27 +1,38 @@
-package com.keadat21v2.movieman.services;
+package dk.keadat21v2.movieman.services;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.*;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
 
-/*
+
 public class Fetcher {
     private String url;
     private String api_token;
+    private List<String> fetchedData = new ArrayList<>();
 
-    public String fetch() throws URISyntaxException {
+    Fetcher(String url){
+        this.url = url;
+        this.api_token = System.getenv("API_TOKEN");
+    }
+
+    public void fetch() throws URISyntaxException {
+        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(url))
                 .GET()
-                .headers("Content-Type", "application/json", "Accept", "application/json")
+                .headers("Content-Type", "application/json", "Accept", "application/json", "Authorization", "Bearer " + api_token)
                 .build();
 
-        HttpClient client = HttpClient.newHttpClient();
+        client.sendAsync(request, BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(fetchedData::add)
+                .join();
+    }
 
-        client.sendAsync(request, BodyHandler.ofString())
-                .thenA
-
-
-
+    public List<String> getFetchedData() {
+        return fetchedData;
     }
 }
-*/
