93e1f721aeec6b97f9b9aaa426ddab79afe3a9c2
BenAtic-KEA <mark024e@stud.kea.dk>
2022-03-23 09:25:18 +0100

    added moviecontroller

---
+++ src/main/java/dk/keadat21v2/movieman/controller/MovieListController.java
@@ -0,0 +1,38 @@
+package dk.keadat21v2.movieman.controller;
+
+import dk.keadat21v2.movieman.dto.MovieResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("api/movielists")
+public class MovieListController {
+
+    /*
+     add movie to List
+     Post Mapping
+
+     */
+
+
+    /*
+    Show all movie lists for a user
+    Get Mapping
+     */
+
+
+    /*
+    show movies from one list
+    Get Mapping
+     */
+
+
+    /*
+    Delete movies from list
+    Delete Mapping
+
+     */
+}
